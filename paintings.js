8b429279c94278af5e90fafb587f3383348143e6
Iuliia Plekhanova <60288386+jules0802@users.noreply.github.com>
2020-06-06 20:16:33 +0400

    add paintings.js

---
+++ paintings.js
@@ -0,0 +1,300 @@
+const paintings1 = [
+  {
+    "id": "1_01",
+    "name": "Stag Hunt",
+    "imageSrc": "files/deerhunt.jpg",
+    "cutSrc": "",
+    "author": "Niccolò dell'",
+    "year": "1550-52"
+  },
+  {
+    "id": "1_02",
+    "name": "Deer beside a Lake",
+    "imageSrc": "files/deerlake.jpg",
+    "cutSrc": "",
+    "author": "Carl Frederik",
+    "year": "1888"
+  },
+  {
+    "id": "1_03",
+    "name": "Country Road with Cypresses",
+    "imageSrc": "files/abbati2.jpg",
+    "cutSrc": "",
+    "author": "ABBATI, Giuseppe",
+    "year": "1860"
+  },
+  {
+    "id": "1_04",
+    "name": "Fireworks in Naples",
+    "imageSrc": "files/firework.jpg",
+    "cutSrc": "",
+    "author": "ACHENBACH, Oswald",
+    "year": "1875"
+  },
+  {
+    "id": "1_05",
+    "name": "The Ninth Wave",
+    "imageSrc": "files/9th_wave.jpg",
+    "cutSrc": "",
+    "author": "AIVAZOVSKY, Ivan Konstantinovich",
+    "year": "1850"
+  },
+  {
+    "id": "1_06",
+    "name": "View of Campo Santi Giovanni e Paolo",
+    "imageSrc": "files/campo.jpg",
+    "cutSrc": "",
+    "author": "ALBOTTO, Francesco",
+    "year": "1745"
+  },
+  {
+    "id": "1_07",
+    "name": "San Giuseppe di Castello",
+    "imageSrc": "files/giuseppe.jpg",
+    "cutSrc": "",
+    "author": "ALBOTTO, Francesco",
+    "year": "1745"
+  },
+  {
+    "id": "1_08",
+    "name": "View of St.Petersburg",
+    "imageSrc": "files/view_stp.jpg",
+    "cutSrc": "",
+    "author": "ALEKSEYEV, Fyodor Yakovlevich",
+    "year": "1795"
+  },
+  {
+    "id": "1_09",
+    "name": "Landscape with the Finding of Moses",
+    "imageSrc": "files/landmose.jpg",
+    "cutSrc": "",
+    "author": "ALLEGRAIN, Etienne",
+    "year": "second half of XVII century"
+  },
+  {
+    "id": "1_10",
+    "name": "River Landscape",
+    "imageSrc": "files/riverla2.jpg",
+    "cutSrc": "",
+    "author": "ALLEGRAIN, Etienne",
+    "year": "second half of XVII century"
+  },
+  {
+    "id": "1_11",
+    "name": "Extensive Wooded Landscape with Cephalus and Procris",
+    "imageSrc": "files/extensiv.jpg",
+    "cutSrc": "",
+    "author": "ALSLOOT, Denis van",
+    "year": "first half of XVII century"
+  },
+  {
+    "id": "1_12",
+    "name": "Skating during Carnival",
+    "imageSrc": "files/skating.jpg",
+    "cutSrc": "",
+    "author": "ALSLOOT, Denis van",
+    "year": "1620"
+  },
+  {
+    "id": "1_13",
+    "name": "Winter Landscape",
+    "imageSrc": "files/winter_l.jpg",
+    "cutSrc": "",
+    "author": "ALSLOOT, Denis van",
+    "year": "1610"
+  },
+  {
+    "id": "1_14",
+    "name": "Winter Landscape in the Fôret de Soignes, with the Flight Into Egypt",
+    "imageSrc": "files/winterla.jpg",
+    "cutSrc": "",
+    "author": "ALSLOOT, Denis van",
+    "year": "1616"
+  },
+  {
+    "id": "1_15",
+    "name": "View of Vienna from the Prater",
+    "imageSrc": "files/viewvien.jpg",
+    "cutSrc": "",
+    "author": "ALT, Rudolf von",
+    "year": "1834"
+  },
+  {
+    "id": "1_16",
+    "name": "An English 3rd - Rate Ship of the Line in Three Positions",
+    "imageSrc": "files/rateship.jpg",
+    "cutSrc": "",
+    "author": "ANDERSON, William",
+    "year": "first half of XIX century"
+  },
+  {
+    "id": "1_17",
+    "name": "Shipping in a Calm Sea off the Coast",
+    "imageSrc": "files/shipcalm.jpg",
+    "cutSrc": "",
+    "author": "ANDERSON, William",
+    "year": "1803"
+  },
+  {
+    "id": "1_18",
+    "name": "Shipping on the Thames off Deptford",
+    "imageSrc": "files/shipping.jpg",
+    "cutSrc": "",
+    "author": "ANDERSON, William",
+    "year": "1825"
+  },
+  {
+    "id": "1_19",
+    "name": "Italianate Coastal Landscape",
+    "imageSrc": "files/landsca3.jpg",
+    "cutSrc": "",
+    "author": "ANESI, Paolo",
+    "year": "XVIII century"
+  },
+  {
+    "id": "1_20",
+    "name": "Tiber Bridge in Rome",
+    "imageSrc": "files/rome.jpg",
+    "cutSrc": "",
+    "author": "ANESI, Paolo",
+    "year": "XVIII century"
+  },
+  {
+    "id": "1_21",
+    "name": "View of the Roman Campagna",
+    "imageSrc": "files/rome1.jpg",
+    "cutSrc": "",
+    "author": "ANESI, Paolo",
+    "year": "XVIII century"
+  },
+  {
+    "id": "1_22",
+    "name": "Harbour Scene",
+    "imageSrc": "files/scene.jpg",
+    "cutSrc": "",
+    "author": "ANESI, Paolo",
+    "year": "XVIII century"
+  },
+  {
+    "id": "1_23",
+    "name": "View of Tivoli and the Roman Campagna",
+    "imageSrc": "files/tivoli.jpg",
+    "cutSrc": "",
+    "author": "ANESI, Paolo",
+    "year": "XVIII century"
+  },
+  {
+    "id": "1_24",
+    "name": "The Western Railway Leaving Paris",
+    "imageSrc": "files/railway.jpg",
+    "cutSrc": "",
+    "author": "ANGRAND, Charles",
+    "year": "1886"
+  },
+  {
+    "id": "1_25",
+    "name": "Landscape with a Musician",
+    "imageSrc": "files/landscap.jpg",
+    "cutSrc": "",
+    "author": "APSHOVEN, Thomas van",
+    "year": "XVII century"
+  },
+  {
+    "id": "1_26",
+    "name": "Edge of a Wood",
+    "imageSrc": "files/edgewood.jpg",
+    "cutSrc": "",
+    "author": "ARTHOIS, Jacques d'",
+    "year": "XVII century"
+  },
+  {
+    "id": "1_27",
+    "name": "An Extensive Wooded Landscape with Travellers on a Path",
+    "imageSrc": "files/extensiv_1.jpg",
+    "cutSrc": "",
+    "author": "ARTHOIS, Jacques d'",
+    "year": "XVII century"
+  },
+  {
+    "id": "1_28",
+    "name": "Wooded Landscape",
+    "imageSrc": "files/landscap_1.jpg",
+    "cutSrc": "",
+    "author": "ARTHOIS, Jacques d'",
+    "year": "XVII century"
+  },
+  {
+    "id": "1_29",
+    "name": "Wooded Landscape",
+    "imageSrc": "files/woodedla.jpg",
+    "cutSrc": "",
+    "author": "ARTHOIS, Jacques d'",
+    "year": "XVII century"
+  },
+  {
+    "id": "1_30",
+    "name": "The Delft City Wall with the Houttuinen",
+    "imageSrc": "files/citywall.jpg",
+    "cutSrc": "",
+    "author": "ASCH, Pieter Jansz.van",
+    "year": "1650"
+  },
+  {
+    "id": "1_31",
+    "name": "River Landscape",
+    "imageSrc": "files/river_la.jpg",
+    "cutSrc": "",
+    "author": "ASCH, Pieter Jansz. van",
+    "year": "1640"
+  },
+  {
+    "id": "1_32",
+    "name": "View of Killarney",
+    "imageSrc": "files/kilarney.jpg",
+    "cutSrc": "",
+    "author": "ASHFORD, William",
+    "year": "1778"
+  },
+  {
+    "id": "1_33",
+    "name": "Landscape with Waterfall and Farm",
+    "imageSrc": "files/waterfal.jpg",
+    "cutSrc": "",
+    "author": "ASSCHE, Henri van",
+    "year": "1806"
+  },
+   {
+    "id": "1_34",
+    "name": "Winter Landscape",
+     "imageSrc": "files/winterla_1.jpg",
+     "cutSrc": "",
+    "author": "ASSCHE, Henri van",
+    "year": "1814"
+  },
+  {
+    "id": "1_35",
+    "name": "Coastal Scene with Resting Riders",
+    "imageSrc": "files/coastal.jpg",
+    "cutSrc": "",
+    "author": "ASSELYN, Jan",
+    "year": "1652"
+  },
+   {
+    "id": "1_36",
+    "name": "Italianate Landscape with a River and an Arched Bridge",
+     "imageSrc": "files/italiana.jpg",
+     "cutSrc": "",
+    "author": "ASSELYN, Jan",
+    "year": "1648"
+  },
+  {
+    "id": "1_37",
+    "name": "Italianate Landscape with Peasants and Animals Fording a River",
+    "imageSrc": "files/italianb.jpg",
+    "cutSrc": "",
+    "author": "ASSELYN, Jan",
+    "year": "1640-45"
+  },
+] 
+
+export default paintings1;
